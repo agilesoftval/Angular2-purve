6b704c9e08ff9fe39da5deb8417236dbc2b467a2
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-07 01:30:37 -0500

    Add controller for Json

---
+++ Angular2-.net-MVC_Angular4/Controllers/BaseAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Angular2_.net_MVC_Angular4.DBContext;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Angular2_.net_MVC_Angular4.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly UserDBEntities UserDB = new UserDBEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
